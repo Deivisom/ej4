23999f28c8b49bffa3d10c7ec05ac8970594fe2d
Deivisom <deivisomandres68@gmail.com>
2021-12-20 13:34:22 +0100

    primer commit de ej4

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.render("index", {title:"Inicio"})
+  })
+app.get("/productos",(req,res)=>{
+    res.render("productos",{title:"Productos"})
+})
+app.use((req, res) => {
+    res.status(404).render("404", { title: "404" });
+  });
+app.listen(PORT,()=>{
+    console.log("iniciando servidor " + PORT)
+})
